645690a1fa59914de06fbb15212278685ffb244c
snehchoudhary <choudharysneha409@gmail.com>
2026-06-11 11:13:00 +0530

    Max_consecutive_1

---
+++ Arrays/Easy/Maximum_Consecutive_1s.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+
+void maximumConsecutive1s (vector<int>&nums){
+    int count = 0;
+    int maxCount = 0;
+
+    for (int i = 0 ; i< nums.size(); i++){
+        if(nums[i] == 1){
+            count++;
+            maxCount = max(count, maxCount);
+        }
+        else {
+            count = 0;
+        }
+    }
+    return maxCount;
+}
+};
