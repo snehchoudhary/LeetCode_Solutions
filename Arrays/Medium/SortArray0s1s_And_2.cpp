bacd3c9a0e7c9bfea42a4d46157f41808a3b518d
snehchoudhary <choudharysneha409@gmail.com>
2026-06-11 12:27:32 +0530

    Sort Array 0 1 and 2

---
+++ Arrays/Medium/SortArray0s1s_And_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortZeroOneTwo(vector<int>& nums) {
+        int index = 0;
+        int left = 0;
+        int right = nums.size()-1;
+
+        while(index <= right){
+            if(nums[index] == 0){
+                swap(nums[index], nums[left]);
+                left++;
+                index++;
+            }
+            else if(nums[index] == 2){
+                swap(nums[index], nums[right]);
+                right--;
+            }
+            else{
+                index++;
+            }
+        }
+    }
+};
