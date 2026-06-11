e2c24e64cc669b1aecb6751ea908cf3eb0432d24
snehchoudhary <choudharysneha409@gmail.com>
2026-06-11 10:31:44 +0530

    UnionOf2SortedArray

---
+++ Arrays/Easy/Union_Of_Two_SortedArray.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+
+vector<int> unionArray(vector<int>&nums1, vector<int>nums2){
+
+    vector<int>result;
+    int i = 0, j = 0;
+
+    while(i < nums1.size() && j < nums2.size()){
+        if(nums1[i] <nums2[j]){
+            if(result.empty() || result.back() != nums1[i]);
+            result.push_back(nums1[i]);
+            i++;
+        }
+        else if (nums2[j] < nums1[i]){
+            if (result.empty() || result.back() != nums2[j]);
+            result.push_back(nums2[j]);
+            j++;
+        }
+        else {
+            if(result.empty() || result.back() != nums1[i]){
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+         // remaining elements of nums1
+        while (i < nums1.size()) {
+            if (result.empty() || result.back() != nums1[i])
+                result.push_back(nums1[i]);
+            i++;
+        }
+
+        // remaining elements of nums2
+        while (j < nums2.size()) {
+            if (result.empty() || result.back() != nums2[j])
+                result.push_back(nums2[j]);
+            j++;
+        }
+
+        return result;
+    }
+}
+};
