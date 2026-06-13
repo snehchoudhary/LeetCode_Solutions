8a8fb1cfed8d061f4145ef8f0b63c50f8baafe02
snehchoudhary <choudharysneha409@gmail.com>
2026-06-13 06:05:02 +0530

    largest Subarray with sum 0

---
+++ Arrays/Medium/Largest_Subarray_WithSum0.cpp
@@ -0,0 +1,41 @@
+//You are given an integer array arr of size n which contains both positive and negative integers. Your task is to find the length of the longest contiguous subarray with sum equal to 0.
+//Return the length of such a subarray. If no such subarray exists, return 0.
+
+//APPROACH : PREFIX SUM + HASHMAP
+//WHY NOT SLIDING WINDOW? Bcoz negative numbers are also present, sliding window can only be reliable only in the case of positive elements only
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxLen(vector<int>& arr) {
+        // Your code goes here
+
+       unordered_map<int, int>mp;
+       int prefixSum = 0;
+       int maxLength = 0;
+
+       for(int i = 0; i < arr.size(); i++){
+        prefixSum = prefixSum +arr[i];
+
+        //if prefix sum itself is a zero
+        if(prefixSum == 0){
+            maxLength =  i + 1;
+        }
+
+        //if this sum was seen before
+        if(mp.find(prefixSum) != mp.end()){
+            int length = i - mp[prefixSum];
+            maxLength = max(maxLength, length);
+        }
+        else{
+            //store first ocurrence only
+            mp[prefixSum] = i;
+        }
+
+       }
+       return maxLength;
+    }
+};
