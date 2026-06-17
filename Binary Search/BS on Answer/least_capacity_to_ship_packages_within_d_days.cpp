74229198b68f058947e0c82165785eb3f416883e
snehchoudhary <choudharysneha409@gmail.com>
2026-06-18 04:14:23 +0530

    last capacityto ship packages within d days

---
+++ Binary Search/BS on Answer/least_capacity_to_ship_packages_within_d_days.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<vector>
+#include<math.h>
+#include<climits>
+#include<algorithm>
+#include<numeric>
+using namespace std;
+
+class Solution {
+public:
+
+    int findDays(vector<int> & weights, int cap){
+        int days = 1, load = 0;
+
+        for(int i = 0; i < weights.size(); i++){
+            if(weights[i] + load > cap){
+                days += 1;
+                load = weights[i];
+            }
+            else{
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while (low <= high){
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+
+            if(numberOfDays <= days){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
