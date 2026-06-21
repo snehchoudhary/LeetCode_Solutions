d899f63eb88083aa7473bb7563a1f617aeebd023
snehchoudhary <choudharysneha409@gmail.com>
2026-06-21 10:54:14 +0530

    insertion at head in LL

--- LinkedList/1D_LL/Insertion_At_Head.cpp
+++ LinkedList/1D_LL/Insertion_At_Head.cpp
@@ -1,4 +1,42 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
+
+/*
+Definition of singly linked list:
+*/
+class ListNode {
+public:
+    int data;
+    ListNode* next;
+
+    ListNode() : data(0), next(nullptr) {}
+
+    ListNode(int x) {
+        data = x;
+        next = nullptr;
+    }
+
+    ListNode(int x, ListNode* next) {
+        data = x;
+        this->next = next;
+    }
+};
+
+
+class Solution {
+public:
+    ListNode* insertAtHead(ListNode* &head, int X) {
+
+        // create new node
+        ListNode* newNode = new ListNode(X);
+
+        // connect new node to old head
+        newNode->next = head;
+
+        // update head
+        head = newNode;
+
+        return head;
+    }
+};
 
