0006dedf50db94f8d374f4b2781675047c108e5d
snehchoudhary <choudharysneha409@gmail.com>
2026-06-22 17:55:28 +0530

    delete node in linked list

---
+++ LinkedList/1D_LL/Deletion_of_head_in_LL.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*
+Definition of singly linked list:
+*/
+class ListNode {
+public:
+    int data;
+    ListNode* next;
+
+    ListNode() : data(0), next(nullptr) {}
+
+    ListNode(int x) {
+        data = x;
+        next = nullptr;
+    }
+
+    ListNode(int x, ListNode* next) {
+        data = x;
+        this->next = next;
+    }
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->data = node->next->data;         // Copy next node's data
+        node->next = node->next->next; 
+    }
+};
