1a4eb14bd8662728ac2d1d283d25c40fcd0bbeab
snehchoudhary <choudharysneha409@gmail.com>
2026-06-29 06:26:21 +0530

    check if number is odd or not

---
+++ Bit Manipulation/easy/check if a number is odd or not.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool isOdd(int n) {
+        // Your code goes here
+    
+            if (n & 1) return true;
+                return false;
+        
+    }
+};
