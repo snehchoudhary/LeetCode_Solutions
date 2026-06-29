2c46a4d942d372b2a5b06c300b8140b7a6fc7811
snehchoudhary <choudharysneha409@gmail.com>
2026-06-30 04:55:37 +0530

    min bit flips to convert a number

---
+++ Bit Manipulation/Interview Qns/min bit flips to convert a number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int ans = start ^ goal;
+
+        int count = 0;
+        for (int i = 0; i < 31; i++){
+            if (ans & (1 << i))
+               count += 1;
+        }
+        return count;
+    }
+};
