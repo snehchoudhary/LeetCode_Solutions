c7358a5cc689a8cad08cedcb2b7a684b776a5467
snehchoudhary <choudharysneha409@gmail.com>
2026-06-30 06:40:02 +0530

    divisor of a number

---
+++ Bit Manipulation/Interview Qns/divisors of a number.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+//BRUTE FORCE APPROACH
+
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int> ans;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+//OPTIMAL APPROACH
+
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int> ans;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                ans.push_back(i);
+
+                if (n / i != i) {
+                    ans.push_back(n / i);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
