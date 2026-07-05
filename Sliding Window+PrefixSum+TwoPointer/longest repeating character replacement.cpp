c3ce66dfa20292d84af806a7d96292730090499a
snehchoudhary <choudharysneha409@gmail.com>
2026-07-05 06:08:04 +0530

    longest repeating character relacement

---
+++ Sliding Window+PrefixSum+TwoPointer/longest repeating character replacement.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector <int>freq(26, 0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength =0;
+
+        for (int right = 0; right < s.size(); right++){
+            freq[s[right] - 'A']++;      //add right character
+
+            //update most frequent character count 
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            //check if window is valid
+            while ((right - left + 1) - maxFreq > k){
+                freq[s[left] - 'A']--;
+                left++;
+            }
+            maxLength = max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};
