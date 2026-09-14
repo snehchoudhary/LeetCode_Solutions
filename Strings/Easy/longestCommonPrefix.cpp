f331e6bca026d43e82f83751a60f2d98a42fcfb6
snehchoudhary <choudharysneha409@gmail.com>
2026-09-14 06:14:00 +0530

    longest common prefix

---
+++ Strings/Easy/longestCommonPrefix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+
+string longestCommonPrefix(vector<string>&strs){
+    
+    if (strs.empty()) return " ";
+
+    for (int i = 0; i <strs[0].length(); i++){
+        char curr = strs[0][i];
+
+        for(int j = 1; j < strs.size(); j++){
+            if ( i >= strs[j].length() || strs[j][i] != curr) {
+                return strs[0].substr(0, i);
+            }
+        }
+    }
+    return strs[0];
+}
+};
