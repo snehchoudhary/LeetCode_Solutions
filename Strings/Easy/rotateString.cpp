3795c9c2a0e0e741e6d76a7fe4c5ceb923a054d3
snehchoudhary <choudharysneha409@gmail.com>
2026-09-16 10:12:51 +0530

    rotateString

---
+++ Strings/Easy/rotateString.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()){
+            return false;
+        }
+        return (s + s).find(goal) != string :: npos;
+    }
+};
